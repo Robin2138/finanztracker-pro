d77b0ce36341c7a4b690724db1aaf44596aa9622
Robin2138 <robinhafner1911@gmail.com>
2026-09-19 10:09:32 +0200

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.FT_CONFIG = {
-  SUPABASE_URL: "https://xxqjvjpedhffvcvcukmz.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_uZbB5CG8Fdd_fFVoosgZXg_TR8uN9Lw",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_uZbB5CG8Fdd_fFVoosgZXg_TR8uN9Lw",
-  APP_NAME: "FinanzTracker",
-  PRO_PRICE_LABEL: "3,99 € / Monat"
-};
